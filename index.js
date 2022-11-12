aaaa5bce261c7efca07b8a7054956fd2227db3b2
Itamar <itamarlucio19@gmail.com>
2022-11-12 08:19:36 -0300

    added some js functions

--- index.js
+++ index.js
@@ -1,4 +1,83 @@
 let sum = 0
 screenText.textContent = sum
+
 function bC() {
+  sum = 0
+  screenText.textContent = sum
+}
+function bS() {
+  sum = sum * sum
+  screenText.textContent = sum
+}
+function bE() {
+  
+  screenText.textContent = sum
+}
+function bD() {
+  
+  screenText.textContent = sum
+}
+function bSe() {
+  
+  screenText.textContent = sum
+}
+function bEi() {
+  
+  screenText.textContent = sum
+}
+function bN() {
+  
+  screenText.textContent = sum
+}
+function bM() {
+  
+  screenText.textContent = sum
+}
+function bF() {
+  
+  screenText.textContent = sum
+}
+function bFi() {
+  
+  screenText.textContent = sum
+}
+function bSi() {
+  
+  screenText.textContent = sum
+}
+function bMi() {
+  
+  screenText.textContent = sum
+}
+function bO() {
+  
+  screenText.textContent = sum
+}
+function bT() {
+  
+  screenText.textContent = sum
+}
+function bTh() {
+  
+  screenText.textContent = sum
+}
+function bP() {
+  
+  screenText.textContent = sum
+}
+function bR() {
+  
+  screenText.textContent = sum
+}
+function bZ() {
+  
+  screenText.textContent = sum
+}
+function bCo() {
+  
+  screenText.textContent = sum
+}
+function bEq() {
+
+  screenText.textContent = sum
 }
